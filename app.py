6f2b75edb1d5d32ce6aee7d4e33ac0ace8329847
Prem Agrawal <premagrawal011@gmail.com>
2025-03-12 23:05:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from config import Config
+from models import db, Task
+
+app = Flask(__name__, template_folder="template")
+app.config.from_object(Config)
+db.init_app(app)
+
+# create tables
+with app.app_context():
+    print("Creating tables...")
+    db.create_all()
+    print("Tables created.")
+
+@app.route('/')
+def index():
+    print("Current working directory:", os.getcwd())
+    print("Templates folder exists:", os.path.exists("templates"))
+    print("Index.html exists:", os.path.exists("templates/index.html"))
+    
+    tasks = Task.query.order_by(Task.created_at.desc()).all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    title = request.form['title']
+    description = request.form['description']
+    new_task = Task(title=title, description=description)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete_task(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
